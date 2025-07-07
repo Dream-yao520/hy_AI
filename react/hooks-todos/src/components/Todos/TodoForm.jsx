5f721b28c96f2e059d72f5801bc6b33c258fdc40
Dream-yao520 <3387210184@qq.com>
2025-07-07 21:31:29 +0800

    Todos 项目

---
+++ react/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react' //私有状态
+
+const TodoForm = () => {
+    return (
+        // JSX 一定要有唯一的最外层元素 树来编译解析JSX，
+        <>
+            <h1 className="header">TodoList</h1>
+            <form action="">
+
+            </form>
+        </>
+    )
+}
+
+export default TodoForm
