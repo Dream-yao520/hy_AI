4f3f646d0ff0ba016b7e58c9281c9c2077071737
Dream-yao520 <3387210184@qq.com>
2025-07-24 21:29:46 +0800

    路由配置和tabbar组件

--- react/trip/src/App.jsx
+++ react/trip/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-
 import './App.css'
+import {
+  Suspense,
+  lazy
+} from 'react'
+import {
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import MainLayout from '@/components/MainLayout'
+import BlankLayout from '@/components/BlankLayout'
+const Home = lazy(() => import('@/pages/Home'))
+const Search = lazy(() => import('@/pages/Search'))
+const Trip = lazy(() => import('@/pages/Trip'))
+const Discount = lazy(() => import('@/pages/Discount'))
+const Collection = lazy(() => import('@/pages/Collection'))
+const Account = lazy(() => import('@/pages/Account'))
 
 function App() {
 
 
   return (
-    <>
-      <div style={{ width: '5.3333rem', height: '5.3333rem', backgroundColor: 'red' }}></div>
-      <div className='box'></div>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      {/* 带有tabbar的Layout */}
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/trip" element={<Trip />} />
+          <Route path="/discount" element={<Discount />} />
+          <Route path="/collection" element={<Collection />} />
+        </Route>
+        {/* 空的Layout */}
+        <Route path="/" element={<BlankLayout />}>
+          <Route path="/search" element={<Search />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
---
+++ react/trip/src/hooks/useTitle.js
@@ -0,0 +1,12 @@
+import {
+    useEffect
+} from 'react'
+
+function useTitle(title) {
+    // 组件加载完成后，设置标题
+    useEffect(() => {
+        document.title = title
+    }, [])
+}
+
+export default useTitle
