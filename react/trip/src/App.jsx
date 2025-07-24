4ca11368c0a80729fb300ede23f7957905d3221d
Dream-yao520 <3387210184@qq.com>
2025-07-24 16:57:53 +0800

    移动端适配等项目初始化

---
+++ react/trip/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div style={{ width: '5.3333rem', height: '5.3333rem', backgroundColor: 'red' }}></div>
+      <div className='box'></div>
+    </>
+  )
+}
+
+export default App
