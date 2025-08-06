28d940d67b5924d109628ca96a92f377cf23db84
Dream-yao520 <3387210184@qq.com>
2025-08-06 11:15:26 +0800

    添加文件cercel.json

--- MyProject/FitKick/src/api/config.js
+++ MyProject/FitKick/src/api/config.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+// 修改前
 axios.defaults.baseURL = 'http://localhost:5173/api'
+
+// 修改后
+// 生产环境使用相对路径，开发环境使用本地地址
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5173/api'
 
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem('token') || ""
