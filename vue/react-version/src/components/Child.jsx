95985fbb019f70f93e3398e7ba3547af112b3152
Dream-yao520 <3387210184@qq.com>
2025-08-14 20:47:10 +0800

    学习 vue

---
+++ vue/react-version/src/components/Child.jsx
@@ -0,0 +1,13 @@
+// 纯UI展示 StatelessComponent  性能优化
+// 无状态
+const Child = (props) => {
+    return (
+        <>
+            <h2>{props.title}</h2>
+        </>
+    )
+}
+
+
+export default Child;
+
---
+++ vue/vue-version/src/hooks/useMousePosition.js
@@ -0,0 +1,30 @@
+import {
+    ref,
+    onMounted,
+    onUnmounted
+} from 'vue'
+
+export function useMousePosition() {
+    const x = ref(0)
+    const y = ref(0)
+
+    const updateMousePosition = (e) => {
+        x.value = e.clientX
+        y.value = e.clientY
+    }
+
+    onMounted(() => {
+        window.addEventListener('mousemove', updateMousePosition)
+    })
+
+    onUnmounted(() => {
+        window.removeEventListener('mousemove', updateMousePosition)
+    })
+
+    return {
+        x,
+        y
+    }
+
+}
+
