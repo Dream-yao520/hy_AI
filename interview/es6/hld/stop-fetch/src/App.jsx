2739640a15417ae14c190d94e8f6017d24adc162
Dream-yao520 <3387210184@qq.com>
2025-08-20 21:28:33 +0800

    终止请求

---
+++ interview/es6/hld/stop-fetch/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  // 实例化一个控制器
+  let controller = new AbortController()
+  useEffect(() => {
+    fetch('http://localhost:5173/api/banners',
+      // 接受信号
+      {
+        signal: controller.signal
+        // signal: AbortSignal.timeout(1000)
+      }
+    ).then(res => res.json()).then(data => {
+      console.log(data)
+    })
+  }, [])
+  // 终止请求
+  const stop = () => {
+    controller.abort()
+  }
+  return (
+    <>
+      <button onClick={stop}>取消请求</button>
+    </>
+  )
+}
+
+export default App
