94b26cdf0bd87b778a43b2bc63d15d214065006d
Dream-yao520 <3387210184@qq.com>
2025-09-11 13:37:01 +0800

    supabase

---
+++ node/supabase/demo/lib/supabaseClient.mjs
@@ -0,0 +1,15 @@
+// Bass 服务模块
+import {
+    createClient
+} from '@supabase/supabase-js'
+import {
+    config
+} from 'dotenv'
+config({
+    path: '.env'
+})
+
+export const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+)
